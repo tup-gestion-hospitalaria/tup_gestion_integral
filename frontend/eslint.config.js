09116df65d0df5ec5d62f1687d85422342dd8c59
Lucas Luciano <luciano-lautaro@hotmail.com>
2026-06-02 00:11:49 -0300

    chore: configure linting tools

---
+++ frontend/eslint.config.js
@@ -0,0 +1,54 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const { defineConfig } = require('eslint/config');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+
+module.exports = defineConfig([
+  {
+    ignores: ['dist/**', 'node_modules/**', '.angular/**']
+  },
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommended,
+      tseslint.configs.stylistic,
+      angular.configs.tsRecommended
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase'
+        }
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case'
+        }
+      ],
+
+      '@angular-eslint/prefer-inject': 'off',
+      '@angular-eslint/template/prefer-control-flow': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off'
+    }
+  },
+  {
+    files: ['**/*.html'],
+    extends: [
+      angular.configs.templateRecommended,
+      angular.configs.templateAccessibility
+    ],
+    rules: {
+      '@angular-eslint/template/prefer-control-flow': 'off'
+    }
+  }
+]);
