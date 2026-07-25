a646cc29307e9520de14d455567a661a7b308863
Lucas Luciano <luciano-lautaro@hotmail.com>
2026-07-24 21:11:23 -0300

    chore(tp11): use hosted API URL

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  backendApiUrl: 'http://localhost:3000/api',
+  backendApiUrl: 'https://tup-gestion-integral.onrender.com/api',
 
   firebaseConfig: {
     apiKey: 'AIzaSyCafv5Un8ifToHe5-I40LM7KG-e9yReRB0',
